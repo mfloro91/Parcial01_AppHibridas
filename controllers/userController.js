48760ad2ce5d51a34af11e466d54f113c0340d15
Flor Rossi <mfloro.91@gmail.com>
2025-05-10 18:29:45 -0300

    coleccion users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import hotelModel from "../models/hotelModel.js";
 
 dotenv.config();
 const secretKey = process.env.JWT_SECRET;
@@ -9,15 +10,21 @@ const secretKey = process.env.JWT_SECRET;
 // Funcion para crear nuevo usuario
 export async function createUser (req, res) {
     try {
-        const {name, userName, email, password} = req.body;
+        const {hotel_id, name, userName, email, role, password} = req.body;
         
         if (!name || !userName || !email || !password) {
             return res.status(400).json({ error: "Todos los campos son obligatorios" });
         }
 
+        // Verificar si el hotel existe
+        const hotel = await hotelModel.findById(hotel_id);
+        if (!hotel) {
+            return res.status(400).json({ error: "Hotel no encontrado" });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
         
-        const user = new userModel({name, userName, email, password: hashedPassword});
+        const user = new userModel({hotel_id, name, userName, email, role, password: hashedPassword});
         const newUser = await user.save();
         
         const {password:_, ...userWithoutPassword} = newUser.toObject();
@@ -31,29 +38,43 @@ export async function createUser (req, res) {
 // Funcion para hacer login de usuarios
 
 export async function loginUser (req, res) {
-    const users = getAllUser();
     const {email, password} = req.body;
 
-    const user = users.find(u => u.email === email);
-    if(!user) {
-        return res.status(404).json({message: "Usuario no encontrado"})
-    }
+    try {
+        const user = await userModel.findOne({ email });
+    
+        if(!user) {
+            return res.status(404).json({message: "Usuario no encontrado"})
+        }
 
     const validPassword = await bcrypt.compare (password, user.password)
     if(!validPassword) {
         return res.status(401).json({message: "Password incorrecto"})
     }
 
-    const token = jwt.sign({id: user.id, email: user.email}, secretKey, {expiresIn: '30min'})
+    const token = jwt.sign(
+        {
+            id: user.id, 
+            email: user.email,
+            role: user.role,
+            hotel_id: user.hotel_id
+        }, 
+        secretKey, 
+        {expiresIn: '30min'}
+    )
 
-    res.status(200).json({token})
+    res.status(200).json({token, role: user.role})
+
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 }
 
 
 // Funcion para obtener todos los usuarios
 export const getAllUsers = async (req, res) => {
     try {
-        const users = await userModel.find().select('-password');
+        const users = await userModel.find().select('-password').populate('hotel_id', 'name logo');
         res.json(users)
     } catch(err) {
         res.status(400).json({error: err.message})
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
 const userSchema = new mongoose.Schema( {
+    hotel_id: {
+        type: Schema.Types.ObjectId, ref:'Hotel',
+        required: true,
+    },
     name: {
         type: String,
         required: true,
@@ -10,6 +14,10 @@ const userSchema = new mongoose.Schema( {
         required: false,
     },
     email: {
+        type: String,
+        required: true,
+    },
+        role: {
         type: String,
         required: true,
     },
